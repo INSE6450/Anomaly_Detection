9a6715c6147df61a3733c499f7f33727ca0fa3e0
PRINCE APPIAH <45251427+wanzima19@users.noreply.github.com>
2026-02-09 15:53:30 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# File system manangement
+import time, psutil, os, gc
+
+# Mathematical functions
+import math
+
+# Data manipulation
+import numpy as np
+import pandas as pd
+
+# Plotting and visualization
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import seaborn as sns
+sns.set_theme()
+
+
